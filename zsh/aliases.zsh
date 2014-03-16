7420b21284838bf68f3b11a7a0724fb1d4214c82
Semen Pupkov <semen.pupkov@gmail.com>
2014-03-16 13:33:34 +0600

    work aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,10 @@
 alias reload!='. ~/.zshrc'
+
+# Work section
 alias wpc='ssh semen@10.101.100.17'
+alias wrails='ssh -L 3000:localhost:3000 10.101.100.17'
+alias wrubik='ssh -L 3010:localhost:3010 10.101.100.17'
+alias rwpc='ssh ssh.railsc.ru -p 22017'
+alias c='cd ~/Sites'
+alias pc='cd ~/Sites/python'
+alias rc='cd ~/Sites/ruby'
