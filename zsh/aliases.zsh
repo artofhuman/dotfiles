c40429cc1dce1b1914059a780220f1b675095c82
Semen Pupkov <semen.pupkov@gmail.com>
2014-09-18 09:52:49 +0600

    change aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,12 +7,12 @@ alias tnew="new-tmux-from-dir-name"
 alias tatt="tmux attach-session -t "
 
 # Work section
-alias wpc='ssh semen@10.101.100.17'
-alias wrails='ssh -L 3000:localhost:3000 10.101.100.17'
-alias wrubik='ssh -L 3010:localhost:3010 10.101.100.17'
-alias wchat='ssh -L 3001:localhost:3001 10.101.100.17'
-alias rwpc='ssh ssh.railsc.ru -p 22017'
-alias rwrails='ssh -L 3000:localhost:3000 ssh.railsc.ru -p 22017'
+alias wpc='ssh artofhuman@10.101.100.17'
+alias wrails='ssh -L 3000:localhost:3000 artofhuman@10.101.100.17'
+alias wrubik='ssh -L 3010:localhost:3010 artofhuman@10.101.100.17'
+alias wchat='ssh -L 3001:localhost:3001 artofhuman@10.101.100.17'
+alias rwpc='ssh artofhuman@ssh.railsc.ru -p 22017'
+alias rwrails='ssh -L 3000:localhost:3000 artofhuman@ssh.railsc.ru -p 22017'
 alias c='cd ~/code'
 alias pc='cd ~/code/python'
 alias rc='cd ~/code/ruby'
