0b62c49e038363defacaf86698c78cbf33c394b7
Semyon Pupkov <mail@semyonpupkov.com>
2017-02-24 00:53:33 +0500

    Add new git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -3,11 +3,12 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
+alias gc='git commit -v'
+alias gca='git commit -a -v'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
 alias gmc='git diff --name-only --diff-filter=U' # show only files with merge conflicts
+alias hpr='hub pull-request'
