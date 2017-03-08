f95666a582fac697f7285903228057df168e83f0
Semyon Pupkov <mail@semyonpupkov.com>
2017-03-08 23:18:26 +0500

    Cleanup aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,16 +6,7 @@ alias td="tmux detach"
 alias tnew="new-tmux-from-dir-name"
 alias tatt="tmux attach-session -t "
 
-# Work section
-alias wpc='ssh artofhuman@10.101.100.17'
-alias wrails='ssh -L 3000:localhost:3000 artofhuman@10.101.100.17'
-alias wrubik='ssh -L 3010:localhost:3010 artofhuman@10.101.100.17'
-alias wchat='ssh -L 3001:localhost:3001 artofhuman@10.101.100.17'
-alias rwpc='ssh artofhuman@ssh.railsc.ru -p 22017'
-alias rwrails='ssh -L 3000:localhost:3000 artofhuman@ssh.railsc.ru -p 22017'
 alias c='cd ~/code'
-alias pc='cd ~/code/python'
-alias rc='cd ~/code/ruby'
 
-alias drun='docker-compose run --service-ports --rm'
+alias dr='docker-compose run --service-ports --rm'
 alias dc='docker-compose'
