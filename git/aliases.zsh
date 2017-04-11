b833938436c29293e7bf1016dd2caf982eefc1af
Semyon Pupkov <mail@semyonpupkov.com>
2017-04-11 10:32:50 +0500

    Add aliases for rebase

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -12,3 +12,8 @@ alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
 alias gmc='git diff --name-only --diff-filter=U' # show only files with merge conflicts
 alias hpr='hub pull-request'
+
+alias rebase='git rebase -i HEAD~2'
+alias rebase3='git rebase -i HEAD~3'
+alias rebase4='git rebase -i HEAD~4'
+alias rebase5='git rebase -i HEAD~5'
