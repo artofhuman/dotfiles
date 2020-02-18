8fa7b2859a16534fa81ae32445ab46c10323713c
Semyon Pupkov <mail@semyonpupkov.com>
2020-02-18 09:49:54 +0500

    Cleanup

--- system/path.zsh
+++
@@ -1,6 +0,0 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH:/usr/local/mysql/bin"
-
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # This loads RVM
-[[ -s "$HOME/.nvm/nvm.sh" ]] && . "$HOME/.nvm/nvm.sh" # This loads NVM
